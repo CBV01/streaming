5b71ae1b3c4bf0803ce57c89926bb246f5c5be70
David A. Victor <codebyvictor01@gmail.com>
2025-11-05 11:33:00 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Stream UK VIP',
+  title: 'StreamMaster',
   description: 'Unlimited Movies, Series & Live TV — Anytime, Anywhere.',
   generator: 'Next.js',
 }
